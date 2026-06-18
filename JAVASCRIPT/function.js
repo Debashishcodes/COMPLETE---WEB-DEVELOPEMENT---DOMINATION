c8524b76a9dd197901b77cef95d4747226631c8f
Debashishcodes <debashishs969@gmail.com>
2026-06-18 22:46:25 +0530

    Day04

---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,23 @@
+// ## Functions
+
+// ## 1 . Function declaration , expression and  Fat arrow function
+
+// ## Type-1: ( function declaration )
+// function lolo(){
+// 	console.log("huhuhuhu");
+// }
+
+// lolo();
+
+// ## Type-2: ( function expression )
+// let lolo = function(){
+// 	console.log("huhuhu");
+// }
+// lolo();
+// // YOU CAN ALSO DECLARE THE FUNCTION NAME THROUGH THE VARIBALE ITS THE FEATURE OF JAVASCRIPT
+
+// ## Type - 3: (Arrow Function)
+// let lolo = ()=>{  
+// 	console.log("deb")
+// }
+// lolo();
