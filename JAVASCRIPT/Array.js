1fc177e3ff33d541114cc2128cd1960e1998f3b1
Debashishcodes <debashishs969@gmail.com>
2026-06-19 20:58:52 +0530

    Day05

---
+++ JAVASCRIPT/Array.js
@@ -0,0 +1,15 @@
+// ## ARRAYS
+// let arr = [1,2,3]; or let arr = new Array();
+// arr[0]=1;
+// arr[10]=undefined;
+
+// # Array methods : push pop shift unshift splice slice reverse sort 
+//  push - add new element at the right most position of array 
+//  pop - remove last element from the array 
+//  shift - remove the first element from the array 
+//  unshift - adds new element at the first position of the array
+//  splice - arr.slice(2,1) : it removes only 1 element at 2nd position of the array 
+//  slice - 
+//  reverse - reverse the entire array 
+//  sort - sort the array in either ascending order or descending order
+//  # map , flter ,recuce, find ,some ,every 
