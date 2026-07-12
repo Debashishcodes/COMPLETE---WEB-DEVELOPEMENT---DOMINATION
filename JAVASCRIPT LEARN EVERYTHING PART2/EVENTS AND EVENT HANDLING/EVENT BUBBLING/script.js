5bb23dc24d942355e656e40b0b0d1c9361d4eb8a
Debashishcodes <debashishs969@gmail.com>
2026-07-12 22:21:34 +0530

    Event Bubbling

---
+++ JAVASCRIPT LEARN EVERYTHING PART2/EVENTS AND EVENT HANDLING/EVENT BUBBLING/script.js
@@ -0,0 +1,12 @@
+let gp = document.getElementById("grand_parent");
+gp.addEventListener("click",()=>{
+    console.log("GRAND PARENT");
+});
+let p = document.getElementById("grand_parent");
+p.addEventListener("click",()=>{
+    console.log("PARENT");
+});
+let child = document.getElementById("child");
+child.addEventListener("click",()=>{
+    console.log("CHILD");
+});
